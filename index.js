4b03416919371c648619baaf2de0319131aca279
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2023-02-10 08:51:38 +0300

    configure port, start script

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ app.get("/", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log(`App is running on port 3000`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Express app is listening on port ${port}`);
 });
