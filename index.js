e20cd5b1493fe7837bffece74dee7c54a1df381c
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2023-02-21 10:33:52 +0300

    revert database, use direct api call

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const customers = require("./routes/customers");
 const topup = require("./routes/topup");
 const withdraw = require("./routes/withdraw");
 
+// models
+const { Customer } = require("./models/customer");
+
 // middlewares
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,8 +29,8 @@ mongoose
   // you can entirely remove the MongoDB Atlas configuration and just use "mongodb://localhost:27017/users" to run it locally in your machine - Kasili Wachiye
   // ==========================================================================================================
   .connect(
-    // "mongodb+srv://kasiliwachiye:uU83hxw7jzkPtmMm@cluster0.koiptqu.mongodb.net/?retryWrites=true&w=majority" ||
-    "mongodb://localhost:27017/users",
+    "mongodb+srv://kasiliwachiye:uU83hxw7jzkPtmMm@cluster0.koiptqu.mongodb.net/?retryWrites=true&w=majority" ||
+      "mongodb://localhost:27017/users",
     { useNewUrlParser: true }
   )
   .then(() => {
@@ -41,6 +44,11 @@ app.get("/", (req, res) => {
   res.send(
     "🔆WELCOME. BIENVENUE. WILKOMMEN.🔆 \n\n To add a customer, proceed to '/api/customers' and make a POST request passing the -name- of the customer. \n\nTo add an account, proceed to '/api/accounts' and make a POST request passing the -name- of the account, the -customerId- (from customer creation - default MongoDB ObjectID) and optionally, the -balance-. \n\nTo add a card, proceed to '/api/cards' and make a POST request passing the customer -accountId-(from account creation - default MongoDB ObjectID) and the (card) -name- and -number-. \n\nTo perform withdrawals and topups, head on to '/api/withdraw' and '/api/topup' respectively and pass in -amount- and -accountId- (from account creation - default MongoDB ObjectID) to withdraw or deposit."
   );
+});
+
+app.get("/customers", async (req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send(customers);
 });
 
 const port = process.env.PORT || 3000;
